8754cf9cc8f23cc64fc7472081e47a6a0aa5f1b1
Mejdi Ourir <m.ourir@ivsweb.com>
2016-01-07 17:48:20 +0000

    device -> client

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ module.exports.Server         = require('./server/');
 module.exports.Client         = {};
 module.exports.Client.TCP     = require('./client/tcp/');
 module.exports.Client.WS     = require('./client/ws/');
+module.exports.cliensManager    = require('./lib/cliensManager');
+
+
